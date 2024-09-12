76cc3fd9e123ab7f57103613c73d776cbfa157b5
Gabriel Luis <gabiluisfreitas@gmail.com>
2024-09-12 15:57:33 +0100

    refactor: separate code into different functions per operating system

--- src/checks/antivirus.ts
+++ src/checks/antivirus.ts
@@ -4,42 +4,54 @@ import { execSync } from "child_process";
 
 const ANTIVIRUS_PROCESSES = "clamav|sophos|eset|comodo|avg|avast|bitdefender";
 
+function checkMacOsAntivirus() {
+  const queries = [
+    "SELECT * FROM xprotect_entries;",
+    "SELECT * FROM xprotect_meta;",
+    "SELECT * FROM launchd WHERE name LIKE '%com.apple.MRT%' OR name LIKE '%com.apple.XProtect%';",
+    "SELECT * FROM processes WHERE name LIKE '%MRT%' OR name LIKE '%XProtect%';",
+  ];
+  for (const q of queries) {
+    const result = executeQuery(q);
+    if (result.length > 0) {
+      return "XProtect/MRT (Built-in macOS protection)";
+    }
+  }
+  return null;
+}
+
+function checkWindowsAntivirus() {
+  const result = execPowershell(
+    `wmic /node:localhost /namespace:\\\\root\\SecurityCenter2 path AntiVirusProduct Get DisplayName | findstr /V /B /C:displayName`
+  ).toString();
+  return result.trim() || null;
+}
+
+function checkLinuxAntivirus() {
+  const processes = execSync(
+    `systemctl list-units --type=service --state=running | grep -i -E '${ANTIVIRUS_PROCESSES}' | awk '{ $1=$2=$3=$4=\"\"; print $0 }'`
+  )
+    .toString()
+    .split("\n")
+    .map((s) => s.trim())
+    .join(", ");
+
+  if (processes) {
+    return processes;
+  }
+  return null;
+}
+
 export function checkAntivirus() {
   const system = os.platform();
   if (system === "darwin") {
-    const queries = [
-      "SELECT * FROM xprotect_entries;",
-      "SELECT * FROM xprotect_meta;",
-      "SELECT * FROM launchd WHERE name LIKE '%com.apple.MRT%' OR name LIKE '%com.apple.XProtect%';",
-      "SELECT * FROM processes WHERE name LIKE '%MRT%' OR name LIKE '%XProtect%';",
-    ];
-    for (const q of queries) {
-      const result = executeQuery(q);
-      if (result.length > 0) {
-        return "XProtect/MRT (Built-in macOS protection)";
-      }
-    }
+    return checkMacOsAntivirus();
   } else if (system === "win32") {
-    const result = execPowershell(
-      `wmic /node:localhost /namespace:\\\\root\\SecurityCenter2 path AntiVirusProduct Get DisplayName | findstr /V /B /C:displayName`
-    ).toString();
-    return result.trim() || null;
+    return checkWindowsAntivirus();
   } else if (system === "linux") {
-    // Search for known antivirus related processes
-    const processes = execSync(
-      `systemctl list-units --type=service --state=running | grep -i -E '${ANTIVIRUS_PROCESSES}' | awk '{ $1=$2=$3=$4=\"\"; print $0 }'`
-    )
-      .toString()
-      .split("\n")
-      .map((s) => s.trim())
-      .join(", ");
-
-    if (processes) {
-      return processes;
-    }
+    return checkLinuxAntivirus();
   }
-
-  return null;
+  throw new Error("Unsupported operating system.");
 }
 
 export function antivirusToString(antivirus: string | null) {
